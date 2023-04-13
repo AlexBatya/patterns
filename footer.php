abc1c5bafca783c06d5498d61e14c28cd4b4fe7c
AlexBatya <alex28.12.12@mail.ru>
2023-04-13 15:48:47 +1000

    newbranch

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer>
+        <div class="footer__body">
+            <div class="footer__body__container container">
+                <h3>Contacts</h3>
+                <h5>Want to know more or just chat? <br> You are welcome!</h5>
+                <button>Send message</button>
+                <div class="messangers">
+                    <img src="<?php bloginfo('template_url') ?>/assets/img/in.svg" alt="">
+                    <img src="<?php bloginfo('template_url') ?>/assets/img/instagram.svg" alt="">
+                    <img src="<?php bloginfo('template_url') ?>/assets/img/be.svg" alt="">
+                    <img src="<?php bloginfo('template_url') ?>/assets/img/ball.svg" alt="">
+                </div>
+                <small>Like me on <br> LinkedIn, Instagram, Behance, Dribble</small>
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: home 
+*/
+?>
+
+<?php get_header(); ?>
+
+
+
+    <main>
+        <div class="main__body">
+            <div class="main__body__container container">
+                
+            </div>
+        </div>
+    </main>
+
+
+<?php get_footer(); ?>
+<?php wp_footer(); ?>
